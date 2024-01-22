1c4681f178f8f909f5271aa420cf681e976ae549
nmacuna <106125117+nmacuna@users.noreply.github.com>
2024-01-22 02:15:02 -0500

    Update random_app.py

--- random_app.py
+++ random_app.py
@@ -9,7 +9,7 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 from scipy.stats import norm, lognorm, weibull_min, gamma, uniform
-from scipy.stats import mode
+from scipy.stats import mode, skew
 from distfit import distfit
 
 def generar_datos_aleatorios():
@@ -82,7 +82,7 @@ def ajustar_distribucion(datos, tipo_distribucion):
     st.write(f"Percentil 25: {round(np.percentile(datos, 25), 3)}")
     st.write(f"Percentil 75: {round(np.percentile(datos, 75), 3)}")
     st.write(f"Rango Interquartílico (IQR): {round(np.percentile(datos, 75) - np.percentile(datos, 25), 3)}")
-    st.write(f"Coeficiente de Asimetría: {round(float(st.skew(datos)), 3)}")
+    st.write(f"Coeficiente de Asimetría: {round(float(skew(datos)), 3)}")
     st.write(f"Curtosis: {round(st.kurtosis(datos), 3)}")
     st.write(f"Entropía: {round(st.entropy(np.histogram(datos, bins='auto')[0]), 3)}")
 
