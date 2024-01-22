b8860a97a8c8c4ba134b4d750a65660506d18419
nmacuna <106125117+nmacuna@users.noreply.github.com>
2024-01-21 22:23:10 -0500

    Update random_app.py

--- random_app.py
+++ random_app.py
@@ -39,7 +39,7 @@ def plot_histogram_and_curve(data, bins, fitted_data, title):
     st.pyplot(fig)
 
 def fit_distribution(data, distribution):
-    params = getattr(stats, distribution).fit(data)
+    params = stats.fit(data, distribution)
     fitted_data = getattr(stats, distribution)(*params).pdf(data)
 
     plot_histogram_and_curve(data, 20, fitted_data, 'Histogram and Fitted Distribution')
