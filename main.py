f4a4e25b79d4019ce73e0e9b1cdfdb6facf48007
nmacuna <106125117+nmacuna@users.noreply.github.com>
2024-01-23 10:52:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,31 +5,33 @@ Created on Sun Jan 21 15:43:45 2024
 @author: nm.acuna
 """
 # main.py
-# main.py
 import streamlit as st
+from PIL import Image
 from correlation_app import app1
-# Import other app modules if needed
+from ProbabilityDistribution_app import app2
+from random_app import app3
 
-# Function to display the banner and menu by default
-def display_banner_and_menu():
-    banner_image = st.image("Confiabilidad_imagen.jpeg", use_column_width=True)
-    st.title("Your App Name")  # Change the app name accordingly
-    return banner_image
+def main():
+    # Show the banner, app name, and navigation menu
+    banner_image = Image.open("Confiabilidad_imagen.jpeg")
+    st.image(banner_image, use_column_width=True)
 
-# Function to display the menu
-def display_menu():
-    st.sidebar.title('Navigation Menu')
-    if st.sidebar.button('App 1'):
-        app1()
-    # Add buttons for other apps if needed
+    st.title("Reliability and Risk Analysis - Visual Resources")
 
-# Function to navigate back to the menu
-def go_to_menu():
-    # Show the banner and menu again
-    st.markdown("<style>div.css-1l02zno{visibility:visible;}</style>", unsafe_allow_html=True)
-    display_banner_and_menu()
+    st.markdown("Developed by Mauricio Sánchez-Silva and Nayled Acuña-Coll for the Reliability and Risk Analysis Course at Universidad de los Andes - Colombia")
 
-# Display the banner and menu by default
-banner_image = display_banner_and_menu()
-# Display the menu
-display_menu()
+    # Navigation menu
+    selected_app = st.sidebar.radio("Select App", ["Home", "Correlation", "Probability Distributions", "Random"])
+
+    # Display selected app content
+    if selected_app == "Home":
+        st.write("Welcome to the main app!")
+    elif selected_app == "Correlation":
+        app1.display_app1()
+    elif selected_app == "Probability Distributions":
+        app2.display_app2()
+    elif selected_app == "Random":
+        app3.display_app3()
+
+if __name__ == "__main__":
+    main()
