04e1b1c65d0358b846e938248e756ad0106fe1c4
nmacuna <106125117+nmacuna@users.noreply.github.com>
2024-01-23 10:31:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ Created on Sun Jan 21 15:43:45 2024
 @author: nm.acuna
 """
 # main.py
+# main.py
 import streamlit as st
 from correlation_app import app1
 # Import other app modules if needed
@@ -20,5 +21,7 @@ def display_menu():
 def go_to_menu():
     display_menu()
 
-# Display the menu by default
+# Display the banner and menu by default
+# You can customize this section further based on your requirements
+banner_image = st.image("Confiabilidad_imagen.jpeg", use_column_width=True)
 display_menu()
