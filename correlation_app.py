698ef94ca752245c6cb480d822ff3836a3bed36c
nmacuna <106125117+nmacuna@users.noreply.github.com>
2024-01-24 10:20:15 -0500

    Update correlation_app.py

--- correlation_app.py
+++ correlation_app.py
@@ -102,10 +102,6 @@ def display_app():
     fig_scatter = plot_scatter_with_regression(x_data, y_data, covariance_matrix)
     st.pyplot(fig_scatter)
     
-
-    st.markdown("Developed by Mauricio Sánchez-Silva and Nayled Acuña-Coll for the Reliability and Risk Analysis course at Universidad de los Andes")
-
-
 if __name__ == "__main__":
     display_app()
 
