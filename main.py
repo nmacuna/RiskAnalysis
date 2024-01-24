ca347a897562199faee17540bc18b6c06d69e9cf
nmacuna <106125117+nmacuna@users.noreply.github.com>
2024-01-24 08:57:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,36 +12,23 @@ import ProbabilityDistribution_app
 import random_app
 
 def main():
-    # Show the banner, app title
+    # Show the banner, app title, and intellectual property description
     banner_image = Image.open("Confiabilidad_imagen.jpeg")
     st.image(banner_image, use_column_width=True)
 
     st.title("Reliability and Risk Analysis - Visual Resources")
 
-    # Get the current page name from the URL
-    page = st.experimental_get_query_params().get("page", [""])[0]
-
-    # If no page is specified, show the selection menu
-    if not page:
-        selected_app = st.slider.radio("Select App", ["Correlation", "Probability Distribution", "Random"])
-
-        # Display selected app content
-        if selected_app == "Correlation":
-            correlation_app.display_app()
-        elif selected_app == "Probability Distribution":
-            distribution_app.display_app()
-        elif selected_app == "Random":
-            random_app.display_app()
-    else:
-        # Display selected app content based on the page parameter
-        if page == "correlation":
-            correlation_app.display_app()
-        elif page == "distribution":
-            distribution_app.display_app()
-        elif page == "random":
-            random_app.display_app()
-
     st.markdown("Developed by Mauricio Sánchez-Silva and Nayled Acuña-Coll for the Reliability and Risk Analysis course at Universidad de los Andes")
+
+    # Display buttons to navigate to each app
+    if st.button("Correlation App"):
+        st.experimental_set_query_params(page="correlation")
+
+    if st.button("Probability Distribution App"):
+        st.experimental_set_query_params(page="distribution")
+
+    if st.button("Random App"):
+        st.experimental_set_query_params(page="random")
 
 if __name__ == "__main__":
     main()
